6e9b3beffc28fc13d750f05807868f022750e0a4
Oteko Patroba <73061955+otekop@users.noreply.github.com>
2023-03-14 10:24:03 +0300

    Updated the config.py
    Used for connecting to the database via SQLAlchemy

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'mysql://root:admin@localhost/vendors'
+    SQLALCHEMY_DATABASE_URI = 'mysql://<<username>>:<<password>>@localhost/<<databasename>>'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = 'modcom'
+    SECRET_KEY = '<<any secret key word>>'
